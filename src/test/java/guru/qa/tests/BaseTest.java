50858b8944c1fbed7b1ae36089d3d47ca73fe320
i.lesnikov <i.lesnikov@bars-group.ru>
2023-12-22 23:34:26 +0300

    Added new tasks

--- src/test/java/guru/qa/tests/BaseTest.java
+++ src/test/java/guru/qa/tests/BaseTest.java
@@ -11,7 +11,7 @@ abstract public class BaseTest {
     public static void setUp() {
         Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
         Configuration.browser = System.getProperty("browser", "Chrome");
-        Configuration.browserVersion = System.getProperty("", "");
+        //Configuration.browserVersion = System.getProperty("", "");
         Configuration.remote = System.getProperty("selenoid" ,"https://user1:1234@selenoid.autotests.cloud/wd/hub");
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
