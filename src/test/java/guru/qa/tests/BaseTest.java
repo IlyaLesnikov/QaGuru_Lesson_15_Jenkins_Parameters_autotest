8bf77964c421ee63ec52b3eda261d1e3f5a5cf6b
i.lesnikov <i.lesnikov@bars-group.ru>
2023-12-23 14:00:31 +0300

    Delete

--- src/test/java/guru/qa/tests/BaseTest.java
+++ src/test/java/guru/qa/tests/BaseTest.java
@@ -12,7 +12,7 @@ abstract public class BaseTest {
         Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
         Configuration.browser = System.getProperty("browser", "Chrome");
         //Configuration.browserVersion = System.getProperty("", "");
-        Configuration.remote = System.getProperty("selenoid" ,"https://user1:1234@selenoid.autotests.cloud/wd/hub");
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
 
