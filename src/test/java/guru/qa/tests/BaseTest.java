05b398dc595f831d3849b8a4b933cc50e42d14e3
i.lesnikov <i.lesnikov@bars-group.ru>
2023-12-24 19:00:42 +0300

    Added new configuration for browser

--- src/test/java/guru/qa/tests/BaseTest.java
+++ src/test/java/guru/qa/tests/BaseTest.java
@@ -11,7 +11,7 @@ abstract public class BaseTest {
     public static void setUp() {
         Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
         Configuration.browser = System.getProperty("browser", "chrome");
-        //Configuration.browserVersion = System.getProperty("", "");
+        Configuration.browserVersion = System.getProperty("version", "100.0.4896.60");
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
